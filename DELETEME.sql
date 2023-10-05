3d5d0c6146757c96322a13ac397b6fac9c27c09c
course-files <137632706+course-files@users.noreply.github.com>
2023-10-05 17:27:33 +0300

    Update DELETEME.sql

--- DELETEME.sql
+++ DELETEME.sql
@@ -1,1 +1,5 @@
+-- The following should be executed so that your account can be identified as you commit and push:
+#  git config --global user.email "you@example.com"
+#  git config --global user.name "Your Name"
+
 CREATE DATABASE IF NOT EXISTS `aomondi_lab2_triggers` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
